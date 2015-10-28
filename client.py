03cc028e1feae08f7314117ae04b4bf2a1fcea49
Shridhar Manvi <Shridhar.Manvi@IgnitionOne.com>
2015-10-27 22:36:31 -0500

    Added working server and client code. Handled GET and POST resuests

--- client.py
+++ client.py
@@ -1,32 +1,81 @@
 import socket
 import urlparse
+import sys
 import re
 import os
 
-socket.setdefaulttimeout = 0.50
-os.environ['no_proxy'] = '127.0.0.1,localhost'
-linkRegex = re.compile('<a\s*href=[\'|"](.*?)[\'"].*?>')
+host = '127.0.0.1'
+port = 8011
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
 CRLF = "\r\n\r\n"
 
-
 def GET(url):
+    #print url
     url = urlparse.urlparse(url)
     path = url.path
-    if path == "":
-        path = "/"
-    HOST = url.netloc  # The remote host
-    PORT = 80          # The same port as used by the server
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.30)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #s.setblocking(0)
-    s.connect((HOST, PORT))
-    s.send("GET / HTTP/1.0%s" % (CRLF))
+    get_statement = 'GET ' + str(path) + 'HTTP/1.0'
+    print get_statement
+    #s.send("GET / HTTP/1.0%s" % (CRLF))
+    s.send("GET " + get_statement)
     data = (s.recv(1000000))
-
-    print data
-    # https://docs.python.org/2/howto/sockets.html#disconnecting
     s.shutdown(1)
     s.close()
     print 'Received', repr(data)
+
+
+
+def GET1(url):
+    url = urlparse.urlparse(url)
+    path = url.path
+
+    headers = "GET " + path + """ HTTP/1.1\r
+    Host: {host}\r
+    Connection: close\r
+    \r\n"""
+
+    header_bytes = headers.format(
+        content_type="application/x-www-form-urlencoded",
+        host=str(host) + ":" + str(port)
+    ).encode('iso-8859-1')
+    send_data = header_bytes
+    s.send(send_data)
+    data_received = (s.recv(1000000))
+    s.shutdown(1)
+    s.close()
+    #print 'Received', repr(data_received)
+    print data_received
+
+
+def POST(body):
+    #host = '127.0.0.1'
+    #port = 8011
+    #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.connect((host, port))
+
+    headers = """\
+    POST /auth HTTP/1.1\r
+    Content-Type: {content_type}\r
+    Content-Length: {content_length}\r
+    Host: {host}\r
+    Connection: close\r
+    \r\n"""
+
+    body = body
+    body_bytes = body.encode('ascii')
+    header_bytes = headers.format(
+        content_type="application/x-www-form-urlencoded",
+        content_length=len(body_bytes),
+        host=str(host) + ":" + str(port)
+    ).encode('iso-8859-1')
+
+    payload = header_bytes + body_bytes
+    s.send(payload)
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'POST':
+        POST('userName=shridhar.manvi&password=manvishridhar')
+    else:
+        if sys.argv[1] == 'GET':
+            GET1('http://127.0.0.1:8010/www/ima')
+
--- server.py
+++ server.py
@@ -2,7 +2,6 @@ __author__ = 'shridharmanvi'
 
 import socket
 
-
 host = ''
 port = 8011
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,39 +9,56 @@ sock.bind((host, port))
 sock.listen(1)  # don't queue up any requests
 
 while True:
-    print 'Server running.....'
+    print 'Server running.....\n'
     csock, caddr = sock.accept()  # Accepts connection
-    print "Connection from: " + str(caddr)
+    #print "Connection from: " + str(caddr)
     message = csock.recv(1024)  # get the request, 1kB max
-    print 'Message: ', message
+    #print 'Message: ', message
     request = message.split() # Main request received from the client
+    #print request[-1]
     request_method = str(request[0])
     filename = str(request[1])
 
-    try:
-        f = open(filename[1:])
-        outputdata = f.read()
-        f.close()
-        #Send one HTTP header line into socket
-        csock.send('HTTP/1.0 200 OK\r\n\r\n')
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            csock.send(outputdata[i])
-        csock.close()
-    except:
-        if filename == '/':
-            csock.send('HTTP/1.0 200 OK\r\n\r\n')
-            f = open('home.html')
-            send_data = f.read()
-            for i in range(0, len(send_data )):
-                csock.send(send_data[i])
+    if request_method == 'GET':
+        try:
+            f = open(filename[1:])
+            outputdata = f.read()
+            f.close()
+            #Send one HTTP header line into socket
+            http_resp = """HTTP/1.0 200 OK\r\n\r\n"""
+
+            #Send the content of the requested file to the client
+            for i in range(0, len(outputdata)):
+                http_resp =http_resp + outputdata[i]
+
+            csock.send(http_resp)
             csock.close()
-        else:
-            csock.send('HTTP/1.1 404 Not Found\r\n\r\n')
-            f = open('file_not_found.html')
-            send_data = f.read()
-            for i in range(0, len(send_data )):
-                csock.send(send_data [i])
+        except:
+            if filename == '/':
+                http_resp = """HTTP/1.0 200 OK\r\n\r\n"""
+                #csock.send('HTTP/1.0 200 OK\r\n\r\n')
+                f = open('home.html')
+                send_data = f.readlines()
+                for i in range(0, len(send_data)):
+                    http_resp = http_resp + send_data[i]
+
+                csock.send(http_resp)
+                csock.close()
+            else:
+                http_resp = """HTTP/1.1 404 Not Found\r\n\r\n"""
+
+                f = open('file_not_found.html')
+                send_data = f.readlines()
+                for i in range(0, len(send_data)):
+                    http_resp = http_resp + send_data[i]
+
+                csock.send(http_resp)
+                csock.close()
+    else:
+        if request_method == 'POST':
+            print message
+            print 'message sent by POST request: \n', request[-1]
             csock.close()
+
 
     csock.close()
